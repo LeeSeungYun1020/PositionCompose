c0d0af622d5d1c37e8cd56e789449fda6ff59882
Lee Seung Yun <ileilliat@gmail.com>
2022-07-13 23:58:02 +0900

    Add location permission view

---
+++ app/src/main/java/com/leeseungyun1020/positioncompose/view/LocationPermissionView.kt
@@ -0,0 +1,17 @@
+package com.leeseungyun1020.positioncompose.view
+
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun LocationPermissionView(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = modifier
+    ) {
+        Text("Location Permission")
+    }
+}
